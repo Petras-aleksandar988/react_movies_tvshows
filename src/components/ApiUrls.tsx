ba912c5d29b550ef83588e83c78c55d92be1d8d4
aleksandar petras <aleksandar.petras@infomedia.ba>
2024-05-10 11:52:55 +0200

    fix issue with api_key env

--- src/components/ApiUrls.tsx
+++ src/components/ApiUrls.tsx
@@ -1,13 +1,12 @@
-const api_key = process.env.REACT_APP_API_KEY
 
 export const movieUrls = {
-    top_10_movies : `https://api.themoviedb.org/3/discover/movie?api_key=${api_key}&sort_by=popularity.desc&page=1&with_original_language=en`,
-    search_movies : `https://api.themoviedb.org/3/search/movie?api_key=${api_key}&language=en-US&query=`
+    top_10_movies : `https://api.themoviedb.org/3/discover/movie?api_key=cbe3b0a2fe19035c9b98fbf6fce9680e&sort_by=popularity.desc&page=1&with_original_language=en`,
+    search_movies : `https://api.themoviedb.org/3/search/movie?api_key=cbe3b0a2fe19035c9b98fbf6fce9680e&language=en-US&query=`
 
    
   };
   export const tvUrls = {
-    top_10_tv_shows : `https://api.themoviedb.org/3/discover/tv?api_key=${api_key}&sort_by=popularity.desc&page=1&with_original_language=en`,
+    top_10_tv_shows : `https://api.themoviedb.org/3/discover/tv?api_key=cbe3b0a2fe19035c9b98fbf6fce9680e&sort_by=popularity.desc&page=1&with_original_language=en`,
     
-    search_tv_shows : `https://api.themoviedb.org/3/search/tv?api_key=${api_key}&language=en-US&query=`
+    search_tv_shows : `https://api.themoviedb.org/3/search/tv?api_key=cbe3b0a2fe19035c9b98fbf6fce9680e&language=en-US&query=`
   };
